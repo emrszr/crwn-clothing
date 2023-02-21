2dae62862d93928745ebfb90b99133517127b557
Sezer Emer <macpro@Sezers-MacBook-Pro.local>
2023-02-21 19:12:28 +0300

    Typescript store/categories completed.

--- src/store/categories/categories.selector.ts
+++ src/store/categories/categories.selector.ts
@@ -1,6 +1,8 @@
 import { createSelector } from "reselect";
+import { CategoriesState } from './categories.reducer';
+import { CategoryMap } from './categories.types';
 
-const selectCategoryReducer = (state) => state.categories;
+const selectCategoryReducer = (state: any): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -9,11 +11,12 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
-    categories.reduce((acc, { title, items }) => {
+  (categories): CategoryMap =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 
